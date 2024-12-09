36e11c33be616cd27ec23768c0177efa27cd43d7
Levan Sarajishvili <lsarajishvili@dga.gov.ge>
2024-12-09 13:04:11 +0400

    resize

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseWindowResizeTest from './components/test';
+
+function App() {
+  return (
+    <div className="App">
+      < UseWindowResizeTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reportWebVitals.js
@@ -0,0 +1,18 @@
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
+
+// src/reportWebVitals.js
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
+
